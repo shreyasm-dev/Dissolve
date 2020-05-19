97bf47a0b965ec6139d26a6ad6b85df4387502e8
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2020-05-19 13:25:37 -0500

    Update dissolvePieChart.js

--- src/split/dissolvePieChart/dissolvePieChart.js
+++ src/split/dissolvePieChart/dissolvePieChart.js
@@ -1,36 +1,50 @@
-class PieChart {
+class dissolvePieChart {
   constructor(slices, elem, rotation = -90) {
-    const svgEl = elem
+    const svgEl = elem;
     let cumulativePercent = 0;
-    svgEl.setAttribute("viewBox", "-1 -1 2 2")
-    svgEl.style.transform = 'rotate(' + rotation + 'deg)'
+    svgEl.setAttribute("viewBox", "-1 -1 2 2");
+    svgEl.style.transform = "rotate(" + rotation + "deg)";
     function getPercent(percent) {
       const x = Math.cos(2 * Math.PI * percent);
       const y = Math.sin(2 * Math.PI * percent);
       return [x, y];
     }
-    slices.forEach(slice => {
+    slices.forEach((slice) => {
       const [startX, startY] = getPercent(cumulativePercent);
       cumulativePercent += slice.percent;
       const [endX, endY] = getPercent(cumulativePercent);
       const largeArcFlag = slice.percent > 0.5 ? 1 : 0;
       const pathData = [
-        `M ${startX} ${startY}`, `A 1 1 0 ${largeArcFlag} 1 ${endX} ${endY}`, `L 0 0`,
-      ].join(' ');
-      const pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-      pathEl.setAttribute('d', pathData);
-      pathEl.setAttribute('fill', slice.color);
+        `M ${startX} ${startY}`,
+        `A 1 1 0 ${largeArcFlag} 1 ${endX} ${endY}`,
+        `L 0 0`,
+      ].join(" ");
+      const pathEl = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "path"
+      );
+      pathEl.setAttribute("d", pathData);
+      pathEl.setAttribute("fill", slice.color);
       for (var loopThroughHoverProps in slice.hover) {
-        pathEl.style.transitionDuration = slice.hover.transitionDuration
-        pathEl.addEventListener('mouseover', function() {
-          pathEl.style[loopThroughHoverProps] = slice.hover[loopThroughHoverProps]
-        }, false)
-        pathEl.addEventListener('mouseleave', function() {
-          pathEl.style[loopThroughHoverProps] = ''
-        }, false)
+        pathEl.style.transitionDuration = slice.hover.transitionDuration;
+        pathEl.addEventListener(
+          "mouseover",
+          function () {
+            pathEl.style[loopThroughHoverProps] =
+              slice.hover[loopThroughHoverProps];
+          },
+          false
+        );
+        pathEl.addEventListener(
+          "mouseleave",
+          function () {
+            pathEl.style[loopThroughHoverProps] = "";
+          },
+          false
+        );
       }
-      svgEl.appendChild(pathEl)
+      svgEl.appendChild(pathEl);
     });
-    return svgEl
+    return svgEl;
   }
 }
